71289cd0fbf7f4f3fab992c704c2232a9d3290bb
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-07-30 14:52:49 -0500

    Complete but in need of logic on finish

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
+
 app.use(expressValidator());
 
 app.use(session({
@@ -14,66 +15,78 @@ app.use(session({
   saveUninitialized: true
 }));
 
+const findLetters = function(word, letter) {
+  const indices = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) === letter) {
+      indices.push(i);
+    }
+  }
+
+  console.log('indices', word, letter, indices);
+  return indices;
+}
+
 //mustache express things.
 const mustacheInstance = mustacheExpress();
 mustacheInstance.cache = null;
+
 app.engine('mustache', mustacheInstance);
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended : true }));
 
-app.use(bodyParser.urlencoded({extended : true}));
-
-
-let lettersGuessed = '';
-let guessesLeft = 0;
-let holderForCorrectLetters = '';
-let randomValue = words;
-
-function generation(req, res){
-  let randomValue = words[Math.floor(words.length * Math.random())];
-
-  req.session.lettersGuessed = lettersGuessed;
-  req.session.guessesLeft = guessesLeft;
-  req.session.randomValue = randomValue;
-  req.session.holderForCorrectLetters = holderForCorrectLetters;
-  console.log("word right now:", randomValue);
-
-  res.render('index', items = {
-    lettersGuessed: req.session.lettersGuessed,
-    guessesLeft: req.session.guessesLeft,
-    randomValue: req.session.randomValue,
-    holderForCorrectLetters: req.session.holderForCorrectLetters
-  });
-}
 // get mustache.index in views and render it to localhost.
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   console.log('getting index');
-  res.render('index');
-  console.log('starting generation function')
-  // generation(req, res);
+
+  req.session.guesses = [];
+  req.session.correctGuesses = [];
+  req.session.guessesLeft = 2;
+  req.session.randomWord = words[ Math.floor(words.length * Math.random()) ];
+  // req.session.randomWord = 'foo';
+
+  const letters = req.session.randomWord.split('');
+  req.session.letters = [];
+  for (let i = 0; i < letters.length; i++) {
+    req.session.letters.push({
+      markup: `<span style="text-decoration: underline">&nbsp;</span>`,
+      letter: letters[i],
+      guessed: false
+    });
+  }
+
+  res.render('index', req.session);
 });
 
-app.post('/', function(req, res){
+app.post('/', function(req, res) {
+  const guess = req.body.guess;
+  req.session.guesses.push(guess);
+  const indices = findLetters(req.session.randomWord, guess);
 
-  let guess = req.body.guess;
+  for (let i = 0; i < indices.length; i++) {
+    const index = indices[i];
+    req.session.letters[index].markup = `<span style="text-decoration:underline">${guess}</span>`;
+    req.session.letters[index].guessed = true;
+  }
 
-  if (randomValue.includes(guess)) {
-    items.lettersGuessed.push(guess)
-    holderForCorrectLetters += guess;
-    guessesLeft -= 1;
+  if (req.session.randomWord.includes(guess)) {
+    req.session.correctGuesses.push(guess);
+  } else {
+    req.session.guessesLeft -= 1;
   }
-  else {
-    guessesLeft -= 1;
-  } if (guessesLeft === 0){
-    holderForCorrectLetters = randomValue;
-    lettersGuessed.clear;
-    lettersGuessed = "You ran out of time!";
+
+  if (!req.session.guessesLeft) {
+    for (let i = 0; i < req.session.letters.length; i++) {
+      if (!req.session.letters[i].guessed) {
+        req.session.letters[i].markup = `<span style="text-decoration:underline red;color:red">${req.session.letters[i].letter}</span>`;
+      }
+    }
   }
-  generation(req, res);
+
+  res.render('index', req.session);
 })
 
-
-
-app.listen(5555, function(){
+app.listen(5555, function() {
   console.log('listening on port 5555!');
 });
